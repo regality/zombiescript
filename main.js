bf61c17da3ce40c5679996695b9b66ca1b1c6492
Adam <regality@gmail.com>
2011-10-15 01:13:07 -0600

    moved link binding to main.js

--- main.js
+++ main.js
@@ -78,7 +78,45 @@ zs.init.init = function () {
    zs.util.require('zombiescript/token');
    zs.init.setupAjax();
    zs.stack.init();
+   zs.init.bindEvents();
 };
+
+zs.init.bindEvents = function() {
+   $("a").live('click', function (e) {
+      var href, re, app, action, data, i, pairs, pair;
+      href = $(this).attr("href");
+      re = href.match(/^\/([a-z_]+)\/?([a-z_]+)?\??(.*)$/);
+      if (re !== null) {
+         e.preventDefault();
+         app = re[1];
+         if (typeof re[2] === "undefined") {
+            if (zs.stack.size(app) === 0) {
+               zs.stack.push(app);
+            } else if (zs.stack.size(app) === 1 &&
+                       $(this).attr("refresh") != "no") {
+               zs.stack.refresh(app);
+            } else {
+               zs.stack.focus(app);
+            }
+         } else {
+            action = re[2];
+            data = {};
+            if (typeof re[3] !== "undefined") {
+               pairs = re[3].split('&');
+               for (i = 0; i < pairs.length; i += 1) {
+                  pair = pairs[i].split('=');
+                  if (typeof pair[1] === "undefined") {
+                     data[pair[0]] = '';
+                  } else {
+                     data[pair[0]] = pair[1];
+                  }
+               }
+            }
+            zs.stack.push(app, action, data);
+         }
+      }
+   });
+}
 
 // setup ajax for the zs
 zs.init.setupAjax = function () {
